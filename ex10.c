a644547c050624031bb3865f781bedc7f3fa6e2a
Nicolas Tamashiro <nicolas.tamashiro19@gmail.com>
2026-03-30 17:14:37 -0300

    colocando verificacao se for 0 ou 1 nao eh primo

--- ex10.c
+++ ex10.c
@@ -7,19 +7,22 @@ int main() {
 
     printf("Digite um numero: ");
 
-
     if(scanf("%d", &n) != 1) {
         printf("Entrada invalida\n");
         return 1;
     }
 
+    // caso seja 0 ou 1
+    if(n < 2) {
+        printf("Nao e primo\n");
+        return 0;
+    }
 
     for(i = 2; i < n; i++) {
         if(n % i == 0) {
             primo = 0;
         }
     }
-
 
     if(primo == 1) {
         printf("E primo\n");
